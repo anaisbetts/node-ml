fbace58f9a6e5312f10da08432cc64315cf7a424
Paul Betts <paul@paulbetts.org>
2018-05-26 19:09:22 -0400

    Implement version

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,10 @@
 const proc = require('child_process');
 const path = require('path');
 
-const params = [require.resolve('./index.ts')].concat([process.argv.slice(2)]);
+// NB: We strip --version because otherwise electron itself will eat it
+const params = [require.resolve('./index.ts')]
+  .concat(process.argv.slice(2).map(x => x === '--version' ? '--version-nodeml' : x))
+
 const cli = path.resolve(require.resolve('electron-prebuilt-compile'), '..', 'cli.js');
 const child = proc.spawn(cli, params, { stdio: 'inherit' });
 
--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,20 @@ import * as yargs from 'yargs';
 
 const args = yargs(process.argv.slice(2))
   .usage('Usage: node-ml [script]')
-  .describe('debug', 'Opens a Devtools window to debug your script')
+  .describe('inspect', 'Opens a Devtools window to debug your script')
+  .describe('version', 'Display version information and exit')
   .alias('h', 'help')
   .argv;
 
 app.on('ready', async () => {
+  if (args['version-nodeml']) {
+    // tslint:disable-next-line:no-require-imports
+    console.log(`node-ml ${require('../package.json').version}\n`);
+    console.log(`Electron Version Info:\n${JSON.stringify(process.versions, null, 2)}`);
+    app.quit();
+    return;
+  }
+
   if (!('_' in args) || args._.length !== 1 || args._[0].length < 1) {
     console.error('Interactive mode not yet supported, specify a script to run\n');
     app.quit();
